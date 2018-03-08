0f8d10ed4df464f53fe6902dd6261b8cf0932565
Lisa Lee <snickers495@gmail.com>
2018-03-08 20:43:21 +0000

    automatically backed up by learn

--- spec/02_services/github_service_spec.rb
+++ spec/02_services/github_service_spec.rb
@@ -16,7 +16,7 @@ describe 'GithubService' do
   describe '#authenticate!' do
     before :each do
       @service = GithubService.new
-      @service.authenticate!(ENV["GITHUB_CLIENT"], ENV["GITHUB_SECRET"], "20")
+      @service.authenticate!(ENV["GITHUB_CLIENT"], ENV["GITHUB_CLIENT_SECRET"], "20")
     end
 
     it "sends the correct POST request" do
